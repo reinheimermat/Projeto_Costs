b0f1f049bb4ca63dffe01c4ec29d683b22ac77fb
reinheimermat <mreinheimer0@gmail.com>
2022-06-01 21:49:45 -0300

    Atualizando a Home.js

--- project/src/components/pages/home/Home.js
+++ project/src/components/pages/home/Home.js
@@ -1,8 +1,17 @@
+import styles from './Home.module.css';
+
+import savings from '../../../img/savings.svg';
+import LinkButton from '../../layout/btn/LinkButton';
+
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <section className={styles.home_container}>
+            <h1>Bem vindo ao <span>Costs</span></h1>
+            <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+            <LinkButton to="/newproject" text="Criar Projeto" />
+            <a href="/">Criar Projeto</a>
+            <img src={savings} alt="Costs" />
+        </section>
     );
 }
 
